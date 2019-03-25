d9fcae5181f6daab2ce8754abff3b508d0f93cfb
prakashsa1 <prakashsa@yahoo.com>
2019-03-25 13:59:01 -0400

    SalesforceFiles

---
+++ src/main/java/com/salesforce/qa/pages/HomePage.java
@@ -0,0 +1,28 @@
+package com.salesforce.qa.pages;
+
+import java.util.Iterator;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
+
+import com.salesforce.qa.base.TestBase;
+import com.salesforce.qa.util.RepositeriesFactory;
+import com.salesforce.qa.util.TestUtil;
+
+public class HomePage extends TestBase{
+
+	public HomePage()
+	{
+		PageFactory.initElements(driver, this);
+	}
+	public NewUsersPage clickOnNewUserLink() throws InterruptedException
+	{
+		Thread.sleep(5000);
+		driver.findElement(By.xpath(RepositeriesFactory.newUser)).click();
+		return new NewUsersPage();
+	}
+}
